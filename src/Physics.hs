b09af3ba569d97d1b1bfa77fa9f74cb26fb4dff2
Nick Hansen <nick.m.hansen@gmail.com>
2017-05-30 23:15:55 -0400

    added history tracking so we can draw a path

--- src/Physics.hs
+++ src/Physics.hs
@@ -10,6 +10,7 @@ import Linear.Vector
 data Particle a = Particle { _particleLocation :: V3 a
                            , _particleVelocity :: V3 a
                            , _particleInvInertia :: a
+                           , _particleHistory :: [V3 a]
                            } deriving Show
 makeLenses ''Particle
 
@@ -19,7 +20,8 @@ eulerStepParticle :: Floating a
                   -> Particle a -- ^ Particle
                   -> V3 a -- ^ Accelleration
                   -> Particle a
-eulerStepParticle deltaT (Particle x0 v0 i) accel = Particle x1 v1 i
+eulerStepParticle deltaT (Particle x0 v0 i path) accel =
+  Particle x1 v1 i (x0:path)
   where
     v1 = v0 ^+^ (accel ^* deltaT)
     x1 = x0 ^+^ (v1 ^* deltaT)
@@ -46,5 +48,9 @@ eulerStepSystem deltaT system = system
                          . map (\f -> f (system ^. systemTime) particle)
                          $ system ^. systemForces )
                        
+trimParticleHistory :: Int -> Particle a -> Particle a
+trimParticleHistory n = particleHistory %~ take n
 
+trimParticleHistories :: Int -> System a -> System a
+trimParticleHistories n = systemParticles . each . particleHistory %~ take n
 
