b42827f5e4c7c6d6c0fc61df75aa11b33a08028f
Nick Hansen <nick.m.hansen@gmail.com>
2017-06-02 21:38:16 -0400

    fixed gloss rendering to deal with z-overlap correctly

--- src/Main.hs
+++ src/Main.hs
@@ -11,12 +11,12 @@ import Render
 import Physics
 
 main :: IO ()
-main = simulate display black 100 system render update
+main = simulate display black 60 system render update
   where
     display = InWindow "demo" (400,400) (10,10)
     system =
       System
-      [ Particle (V3 100 100 0) (V3 (-2) 0 0) 8000 []
+      [ Particle (V3 80 80 0) (V3 (-2) 0 2) 7000 []
       , Particle (V3 40 0 40) (V3 0 10 0) 5600 [] ]
       [force]
       0
@@ -24,24 +24,18 @@ main = simulate display black 100 system render update
     force t p =
       ((p^.particleInvInertia)/(quadrance$p^.particleLocation)) *^
       (signorm.negated$p^.particleLocation)
-    render (System [p1,p2] _ _ ) = pictures
-                                   [ drawPathGloss
-                                     projM
-                                     (400,400)
-                                     (1,0,0,1)
-                                     (p1^.particleHistory)
-                                   , drawPathGloss
-                                     projM
-                                     (400,400)
-                                     (0,1,0,1)
-                                     (p2^.particleHistory) ]
+    render (System [p1,p2] _ _ ) = drawPathsGloss 100 projM (400,400) 
+                                   [ ( (1,0,0,1),
+                                       (p1^.particleHistory) )
+                                   , ( (0,1,0,1),
+                                     (p2^.particleHistory) ) ]
     projM = projectionMatrix
             (V3 20 0 100)
             (V3 0 0 0)
             (V3 30 0 100)
             (pi/1.5)
             1
-    update _ = (trimParticleHistories 400 <$>) <$> eulerStepSystem
+    update _ = (trimParticleHistories 1100 <$>) <$> eulerStepSystem
     
     
 
--- src/Render.hs
+++ src/Render.hs
@@ -1,14 +1,13 @@
 module Render
   ( RGBA
   , projectionMatrix
-  , projectPoint
+--  , projectPoint
   , drawPathCairo
-  , drawPathGloss
+  , drawPathsGloss
   ) where
 
 import ClassyPrelude hiding (point)
 import Control.Lens
-import GHC.Float (double2Float)
 import Graphics.Gloss
 import Graphics.Rendering.Cairo
 import Linear.Epsilon (Epsilon)
@@ -18,6 +17,7 @@ import Linear.V2
 import Linear.V3
 import Linear.V4
 import Linear.Vector
+
 
 --- Linear algebra stuff ---
 
@@ -36,12 +36,56 @@ projectionMatrix eye focus up fov aspect = perspectiveM !*! viewM
 projectPoint :: Floating a
              => M44 a -- ^ Projection matrix
              -> V3 a -- ^ Point to project
-             -> V2 a
-projectPoint projM = view _xy
-                     . normalizePoint
+             -> V3 a
+projectPoint projM = normalizePoint
                      . (projM !*)
-                     . point
-                     
+                     . point                    
+
+projectPoint' :: Floating a
+              => M44 a -- ^ Projection matrix
+              -> V3 a -- ^ Point to project
+              -> V2 a
+projectPoint' = (view _xy .) <$> projectPoint
+
+
+--- Path drawing ---
+
+-- | Divide a list into overlapping chunks
+chunks :: Int -> [a] -> [[a]]
+chunks _ [] = []
+chunks chunkSize xs | chunkSize < 2 = [xs]
+                    | otherwise = front : chunks chunkSize back
+  where
+    front = take chunkSize xs
+    back = drop (chunkSize-1) xs
+
+-- | Project a bunch of paths based on a provided matrix, break them into chunks,
+-- then sort them based on their z-dimension and render them with the provided
+-- renderer.
+renderPaths :: (Floating a, Ord a)
+            => Int -- ^ Segment size (to handle overlap)
+            -> (RGBA a -> [V2 a] -> r) -- ^ Rendering function
+            -> M44 a -- ^ Projection matrix
+            -> [(RGBA a,[V3 a])] -- ^ Paths to render
+            -> [r]
+renderPaths segmentSize render projM paths = map (uncurry render) orderedSegments
+  where
+    orderedSegments = map ( second $ map ( view _xy ) )
+                      . sortBy compareSegment
+                      $ unorderedSegments                    
+    compareSegment (_, a) (_, b) =
+      case (a, b) of
+        ([], _ ) -> LT
+        (_, [] ) -> GT
+        ((V3 _ _ z1:_ ), (V3 _ _ z2:_ )) -> compare z1 z2
+    unorderedSegments =
+      concatMap
+      (\(color,path) -> map ((,) color)
+                        . chunks segmentSize
+                        . map (projectPoint projM)
+                        $ path )
+      paths
+            
 
 --- Cairo rendering ---
 
@@ -58,7 +102,7 @@ drawPathCairo projM (width,height) (colorR,colorG,colorB,colorA) path = do
   setSourceRGBA colorR colorG colorB colorA
   setLineCap LineCapRound
   setLineJoin LineJoinRound
-  let (start:rest) = map (\pt -> let pt' = projectPoint projM pt
+  let (start:rest) = map (\pt -> let pt' = projectPoint' projM pt
                                  in ((view _x pt + 1) * width/2,
                                      (view _y pt + 1) * height/2))
                      path
@@ -86,32 +130,41 @@ cairoTest = do
 
 
 --- Gloss rendering ---
-drawPathGloss :: M44 Float -- ^ Projection matrix
-              -> (Float,Float) -- ^ (width,height)
-              -> RGBA Float -- ^ Path color
-              -> [V3 Float] -- ^ 2d path
-              -> Picture
-drawPathGloss projM (width,height) (colorR,colorG,colorB,colorA) path =
+
+-- | Draw a 3-d path given a projection matrix.
+drawPathGloss' :: M44 Float -- ^ Projection matrix
+               -> (Float,Float) -- ^ (width,height)
+               -> RGBA Float -- ^ Path color
+               -> [V3 Float] -- ^ 2d path
+               -> Picture
+drawPathGloss' projM (width,height) (colorR,colorG,colorB,colorA) path =
   color myColor myPath
   where
     myColor = makeColor colorR colorG colorB colorA
     myPath = line $
              map ( (\(V2 x y) -> (x*width/2, y*height/2)) .
-                   projectPoint projM )
+                   projectPoint' projM )
              path
 
-glossTest :: IO ()
-glossTest = display d black p
+-- | Draw a 2-d path.
+drawPathGloss :: (Float,Float) -- ^ (width,height)
+              -> RGBA Float -- ^ Path color
+              -> [V2 Float] -- ^ 2d path, coordinates [-1..1]
+              -> Picture
+drawPathGloss (width,height) (colorR,colorG,colorB,colorA) path =
+  color myColor myPath
   where
-    d = InWindow "test" (400,400) (10,10)
-    p = drawPathGloss projM (400,400) (0.8,0.5,0.0,1.0) $
-        [ V3 0 0 0
-        , V3 1 0 0
-        , V3 0 1 5 ]
-    projM = projectionMatrix
-            (V3 0 0 10)
-            (V3 0 0 0)
-            (V3 0 1 10)
-            (pi/3)
-            1
+    myColor = makeColor colorR colorG colorB colorA
+    myPath = line $
+             map (\(V2 x y) -> (x*width/2, y*height/2))
+             path
 
+-- | Draw a bunch of paths together, segmenting them so that overlaps are
+-- correctly drawn.
+drawPathsGloss :: Int -- ^ Segment size
+               -> M44 Float -- ^ Projection matrix
+               -> (Float,Float) -- ^ Width, height
+               -> [(RGBA Float, [V3 Float])] -- ^ Paths to draw
+               -> Picture
+drawPathsGloss segmentSize projM dims =
+  pictures . renderPaths segmentSize (drawPathGloss dims) projM
